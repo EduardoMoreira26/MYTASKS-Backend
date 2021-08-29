bbc3263e1cfa0ddbe29608ea74336d46cb3ada09
Eduardo Moreira <62812791+EduardoMoreira26@users.noreply.github.com>
2021-08-29 19:32:39 -0300

    commit

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { User } from './../auth/user.entity';
+import { TaskStatus } from './task-status.enum';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
+  user: User;
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,73 @@
+import { User } from './../auth/user.entity';
+import { Task } from './task.entity';
+import { UpdateTaskStatusDto } from './dto/update-task.status.dto';
+import { GetTasksFiltersDto } from './dto/get-tasks-filter.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { Logger } from '@nestjs/common';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+  private logger = new Logger('TasksController');
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(
+    @Query() filterDto: GetTasksFiltersDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
+    return this.tasksService.getTasks(filterDto, user);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string, @GetUser() user: User): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
+  }
+
+  @Post()
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(
+        createTaskDto,
+      )}`,
+    );
+    return this.tasksService.createTask(createTaskDto, user);
+  }
+
+  @Delete('/:id')
+  deteleTask(@Param('id') id: string, @GetUser() user: User): Promise<void> {
+    return this.tasksService.deteleTask(id, user);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    const { status } = updateTaskStatusDto;
+    return this.tasksService.updateTaskStatus(id, status, user);
+  }
+}
